2d35faa6a891a80cc64e3b10401c8cd185d5c344
Sagar Dagdu <shags032@gmail.com>
2024-05-02 16:22:42 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,9 +8,6 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "FairmaticSDK",
-            targets: ["FairmaticSDK"]),
-        .library(
             name: "FairmaticSDK-Target",
             targets: ["FairmaticSDK-Target"]
         )
